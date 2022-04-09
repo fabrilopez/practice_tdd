7a477e8886fd044a32272e2a48df6dd465abc548
Fabricio <lopezfabricio1978@gmail.com>
2022-04-09 19:37:39 -0300

    more test

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,9 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
-
 
 class HomePageTest(TestCase):
 
@@ -12,9 +12,16 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
+
+        # request = HttpRequest()
+        # response = home_page(request)
         html = response.content.decode('utf8')
+        # expected_html = render_to_string('home.html')
+        # self.assertEqual(html, expected_html)
+
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip('\n').endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
