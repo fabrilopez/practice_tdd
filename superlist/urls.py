2da5eede0534911ab84b8dd6fe475e17d8645dbf
Fabricio <lopezfabricio1978@gmail.com>
2022-04-11 18:17:47 -0300

    End part I book

--- superlist/urls.py
+++ superlist/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, re_path
-
-
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    re_path(r'^$', views.home_page, name='home'),
+    re_path(r'^$', list_views.home_page, name='home'),
+    re_path(r'^lists/', include(list_urls)),
 ]
